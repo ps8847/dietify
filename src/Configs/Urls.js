6f738c363d61f52b25c055bfc46ce56af576f994
Prince Sharma <ps789650@gmail.com>
2024-10-10 23:09:03 +0530

    made necessary changes

--- src/Configs/Urls.js
+++ src/Configs/Urls.js
@@ -2,4 +2,4 @@
 const DEVELOPMENT_URL = "http://127.0.0.1:8000/api/";
 const PRODUCTION_URL = "https://doctorbackend.mhtm.ca/api/";
 
-export const MAIN_URL = DEVELOPMENT_URL;
+export const MAIN_URL = PRODUCTION_URL;
